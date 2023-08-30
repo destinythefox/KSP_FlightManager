464eddf655eb2d9db1b175e285ee3fc3ab1dc11b
adrianmac <adrianmaciaszek96@gmail.com>
2023-08-30 02:02:56 +0100

    deleted:    main.py

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from flask import Flask, jsonify, request
 from db.database import FlightDatabase
+import random
 
 app = Flask(__name__)
 db = FlightDatabase('KSP_FlightManager.db')
 
-@app.route('/flights', methods=['GET'])
+def generate_launch_id():
+    return f"UKSA-{random.randint(1000, 9999)}"
+
+@app.route('/get_flights', methods=['GET'])
 def get_flights():
     flights = db.get_flights()
-    return jsonify(flights)
+    return jsonify(flights=flights)
 
-@app.route('/flights', methods=['POST'])
-def add_flight():
-    data = request.json
-    db.insert_flight(data)
-    return jsonify({"message": "Flight added"}), 201
+@app.route('/insert_flight', methods=['POST'])
+def insert_flight():
+    launch_id = generate_launch_id()
+    lv_name = request.json.get('lv_name', '')
+    payload = request.json.get('payload', '')
+    phase = request.json.get('phase', '')
+    destination = request.json.get('destination', '')
+    manned = request.json.get('manned', False)
+    current_status = request.json.get('current_status', '')
+    flight_recorder_data = request.json.get('flight_recorder_data', '')
+    failures = request.json.get('failures', '')
+    comments = request.json.get('comments', '')
 
-@app.route('/flights/<string:launch_id>', methods=['GET'])
+    db.insert_flight((launch_id, lv_name, payload, phase, destination, manned, current_status, flight_recorder_data, failures, comments))
+    return jsonify(status='success', launch_id=launch_id)
+
+@app.route('/get_single_flight/<string:launch_id>', methods=['GET'])
 def get_single_flight(launch_id):
     flight = db.get_single_flight(launch_id)
     if flight:
-        return jsonify(flight)
+        return jsonify(flight=flight)
     else:
-        return jsonify({"message": "Flight not found"}), 404
+        return jsonify(error='Flight not found'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
